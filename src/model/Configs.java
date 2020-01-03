50234392e3cea5c0e8f4cf89cdec6aedcbcb74e1
harshrajdhote <dhoteharshraj@gmail.com>
2020-01-03 16:47:17 +0530

    Version 1.0

---
+++ src/model/Configs.java
@@ -0,0 +1,11 @@
+package model;
+
+
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "@dalton12";
+    protected String dbName = "scheduler";
+}
