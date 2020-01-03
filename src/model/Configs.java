b576fea9470491075055b63531bab7749b8bc912
harshrajdhote <dhoteharshraj@gmail.com>
2020-01-03 17:02:38 +0530

    DB file added

--- src/model/Configs.java
+++ src/model/Configs.java
@@ -6,6 +6,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "@dalton12";
+    protected String dbPass = "yourpass";
     protected String dbName = "scheduler";
 }
